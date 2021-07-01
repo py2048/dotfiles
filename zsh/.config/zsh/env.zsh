b956baa14a804eb7f058556861cd0a236eb92c08
py1024 <quannhn2001@gmail.com>
2021-07-01 19:01:32 +0700

    Auto install plugins

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -1,24 +1,33 @@
 # Set PATH
-export PATH="$HOME/.local/bin:$HOME/.cargo/bin:$HOME/.npm-global/bin:$HOME/go/bin:$PATH"
+
+# User executable
+export PATH="$HOME/.local/bin:$PATH"
+# Rust binaries
+export PATH="$HOME/.cargo/bin:$PATH"
+# Npm global packages
+export PATH="$HOME/.npm-global/bin:$PATH"
+# Go binaries
+export PATH="$HOME/go/bin:$PATH"
+
 
 # MANPAGER
 # export MANPATH="/usr/local/man:$MANPATH"
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
-# You may need to manually set your language environment
+# Set language environment
 export LANG=en_US.UTF-8
 
 # Set editor
 export EDITOR='nvim'
 export VISUAL="nvim"
 
-# Change starhip config location
-export STARSHIP_CONFIG=~/.config/starship/starship.toml
 
 # User aliases
 
-alias v="nvim"
+# Neovim aliases
+alias v='nvim'
 # alias vim="~/Apps/nvcode/nvim"
+alias nv='cd ~/.config/nvim && nvim ~/.config/nvim/init.vim'
 
 # Lsd replace for ls
 alias ls='lsd'
@@ -26,8 +35,7 @@ alias la='ls -a'
 alias l='ls -la'
 # Vim bindings
 alias :q='exit'
-alias p='nvim ~/.zshrc'
-# alias nv='nvim ~/.config/nvim/init.vim'
+alias p="nvim $ZDOTDIR/.zshrc"
 
 # Tree
 alias tree='tree -C'
@@ -39,6 +47,9 @@ alias k="v ~/.config/kitty/kitty.conf"
 alias gc="git clone"
 alias gp="git pull"
 
+# Open
+type open > /dev/null || alias open='xdg-open'
+
 
 # Set env for intel
 export FEAPHOME8_4=/home/py1024/FEAP/ver84
@@ -47,9 +58,26 @@ source /opt/intel/oneapi/mkl/latest/env/vars.sh
 source /opt/intel/oneapi/mpi/latest/env/vars.sh
 
 
-# print out which 
+# Custom function
+
+# Print out which 
 bwhich(){
     bat $(which $1)
 }
 compdef _path_commands bwhich 
 
+# Update zsh plugin
+plug_update(){
+    find "$ZDOTDIR/plugins" -type d -exec test -e '{}/.git' ';' -print0 | xargs -I {} -0 git -C {} pull
+}
+
+function z_plug() {
+    PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
+    if [ -d "$ZDOTDIR/plugins/$PLUGIN_NAME" ]; then 
+        # For plugins
+        source "$ZDOTDIR/plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh" || \
+        source "$ZDOTDIR/plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh"
+    else
+        git clone "https://github.com/$1.git" "$ZDOTDIR/plugins/$PLUGIN_NAME"
+    fi
+}
--- zsh/.config/zsh/plugin.zsh
+++ zsh/.config/zsh/plugin.zsh
@@ -8,26 +8,26 @@ source ~/.config/nnn/nnn.zsh
 source ~/.config/lf/icons.sh
 
 # zsh plugins
-source ~/Apps/zsh-autosuggestions/zsh-autosuggestions.zsh
-source ~/Apps/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+z_plug "zsh-users/zsh-autosuggestions"
+z_plug "zsh-users/zsh-syntax-highlighting"
 
 # z.lua
 eval "$(lua $HOME/Apps/z.lua/z.lua --init zsh enhanced once echo)"
+# Change zlua cache location
 export _ZL_DATA=$HOME/.cache/zlua
 
+# fzf
 source ~/.config/fzf/general.zsh
 source ~/.config/fzf/key-bindings.zsh
-# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
-# --color hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
-# --color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B
-# '
 
 # Activate conda environment
-# conda()
-# {
-#     source /path/to/conda/env.zsh
-#     conda activate base
-# }
+conda()
+{
+    [ -f ~/.miniconda3/etc/profile.d/conda.sh ] && source ~/.miniconda3/etc/profile.d/conda.sh
+    conda activate base
+}
 
+# Change starhip config location
+export STARSHIP_CONFIG=~/.config/starship/starship.toml
 # Starship prompt
 eval "$(starship init zsh)"
