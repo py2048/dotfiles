545f73728fbdd76ecd00586e392a1ec4f698285b
py2048 <quannhn2001@gmail.com>
2021-10-09 23:59:57 +0700

    New cmp config

--- nvim/.config/nvim/lua/nvim_cmp.lua
+++ nvim/.config/nvim/lua/nvim_cmp.lua
@@ -6,12 +6,16 @@
 
 local cmp = require'cmp'
 
+-- Highlight
+vim.api.nvim_command('hi CmpItemKind ctermfg=White')
+vim.api.nvim_command('hi CmpItemMenu ctermfg=White')
+
 vim.opt.completeopt = "menuone,noselect"
 
-local check_back_space = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col == 0 or vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') ~= nil
-end
+-- local check_back_space = function()
+--     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+--     return col == 0 or vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') ~= nil
+-- end
 
 -- nvim-cmp setup
 cmp.setup {
@@ -56,10 +60,12 @@ cmp.setup {
         --     select = true,
         -- },
         ['<Tab>'] = function(fallback)
-            if vim.fn.pumvisible() == 1 then
-                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n', true)
-            elseif check_back_space() then
-                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, true, true), 'n', true)
+            if cmp.visible() then
+                cmp.select_next_item()
+            -- elseif check_back_space() then
+            --     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, true, true), 'n', true)
+            elseif has_words_before() then
+                cmp.complete()
             -- elseif vim.fn['vsnip#available']() == 1 then
             --     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Plug>(vsnip-expand-or-jump)', true, true, true), '', true)
             else
@@ -67,10 +73,10 @@ cmp.setup {
             end
         end,
         ['<S-Tab>'] = function(fallback)
-            if vim.fn.pumvisible() == 1 then
-                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-o>', true, true, true), 'n', true)
-            elseif check_back_space() then
-                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<S-Tab>', true, true, true), 'n', true)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            -- elseif check_back_space() then
+            --     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<S-Tab>', true, true, true), 'n', true)
             -- elseif vim.fn['vsnip#available']() == 1 then
             --     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Plug>(vsnip-expand-or-jump)', true, true, true), '', true)
             else
