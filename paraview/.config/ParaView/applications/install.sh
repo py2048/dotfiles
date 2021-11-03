1ea0df6bce93031040ab7141dec51213edb27bd2
py2048 <quannhn2001@gmail.com>
2021-11-03 18:04:43 +0700

    Paraview desktop application linux

---
+++ paraview/.config/ParaView/applications/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir -p ~/.local/share/applications
+mkdir -p ~/.local/share/mime/packages
+
+sudo ln -s ~/Apps/ParaView*/bin/paraview /usr/local/bin/paraview
+
+cp ./paraview.desktop ~/.local/share/applications/paraview.desktop
+
+cp ./x-paraview.xml ~/.local/share/mime/packages/x-paraview.xml
+
+update-desktop-database ~/.local/share/applications
+update-mime-database    ~/.local/share/mime
