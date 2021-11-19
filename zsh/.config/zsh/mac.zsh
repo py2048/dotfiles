f1d0b31c041debd43e7edbe8b78404979ee1e5f8
py2048 <quannhn2001@gmail.com>
2021-11-20 00:20:17 +0700

    mac os

--- zsh/.config/zsh/mac.zsh
+++ zsh/.config/zsh/mac.zsh
@@ -20,6 +20,9 @@ add_path /Applications/ParaView-5.9.1.app/Contents/bin
 # Drag and drop in MacOS
 alias dr="open -a /Applications/Dropover.app/Contents/MacOS/Dropover"
 
+# rclonbe exclude .DS_Store
+alias rclone='rclone --exclude ".DS_Store"'
+
 # Recent mpv
 mpv_rc() {
     tac ~/.cache/mpv_history | fzf | awk '{split($0,a,"] /"); print "/" a[2]}' | xargs -I {} open '{}'
