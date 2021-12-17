b30d17c60237658afad88ffe4a52923f68409273
py2048 <quannhn2001@gmail.com>
2021-12-17 19:30:01 +0700

    Fix mpv regex

--- zsh/.config/zsh/mac.zsh
+++ zsh/.config/zsh/mac.zsh
@@ -6,15 +6,12 @@ add_path $HOME/Library/Python/3.9/bin
 # Mac OS coreutils
 add_path /usr/local/opt/coreutils/libexec/gnubin
 
+# Add curl PATH
+add_path /usr/local/opt/curl/bin
+
 # Paraview PATH
 add_path /Applications/ParaView-5.9.1.app/Contents/bin
 
-
-# ifort PATH
-# if [ -d "/opt/intel/oneapi" ]; then
-#     source /opt/intel/oneapi/compiler/latest/env/vars.sh
-#     alias ifort='ifort -L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib'
-# fi
 
 # Aliases
 # Drag and drop in MacOS
@@ -25,7 +22,7 @@ alias rclone='rclone --exclude ".DS_Store"'
 
 # Recent mpv
 mpv_rc() {
-    tac ~/.cache/mpv_history | fzf | awk '{split($0,a,"] /"); print "/" a[2]}' | xargs -I {} open '{}'
+    tac ~/.cache/mpv_history | fzf | grep -oP '^\[\d{2}/\w{3}/\d{2}\s\d{2}:\d{2}:\d{2}\]\s\K.+' | xargs -I {} open '{}'
 }
 
 # Plugins
