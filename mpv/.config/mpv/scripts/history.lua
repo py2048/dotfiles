464189ba83c1dd63d64b3030282ace62a1f8bf00
py2048 <quannhn2001@gmail.com>
2022-01-05 00:57:25 +0700

    Fix mpv

--- mpv/.config/mpv/scripts/history.lua
+++ mpv/.config/mpv/scripts/history.lua
@@ -3,11 +3,15 @@
 -- Save as "mpvhistory.lua" in your mpv scripts dir. Log will be saved to mpv default config directory.
 -- Make sure to leave a comment if you make any improvements/changes to the script!
 
-local HISTFILE = os.getenv('HOME')..'/.cache/mpv_history';
+local HISTFILE = os.getenv('HOME')..'/.cache/mpv_history'
 
 mp.register_event('file-loaded', function()
-    local logfile;
-    logfile = io.open(HISTFILE, 'a+');
-    logfile:write(('[%s] %s\n'):format(os.date('%d/%b/%y %X'), mp.get_property('path')));    
-    logfile:close();
+    local logfile
+    local path = mp.get_property('path')
+    if path:sub(1, 1) ~= '/' then
+        path = mp.get_property('working-directory') .. '/' .. path
+    end
+    logfile = io.open(HISTFILE, 'a+')
+    logfile:write(('[%s] %s\n'):format(os.date('%d/%b/%y %X'), path))
+    logfile:close()
 end)
