33e18a0fe3722235e4dc4b7a867100d826ee9990
py2048 <quannhn2001@gmail.com>
2022-01-09 00:03:28 +0700

    Add goldendict and fix

---
+++ build/mac/mac_qtsetup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+export Qt5_DIR=/usr/local/opt/qt5/lib/cmake/Qt5/
+export Qt5LinguistTools_DIR=/usr/local/opt/qt5/lib/cmake/Qt5LinguistTools
+export DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer/
