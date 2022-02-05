5a2d8bbc834bfc54b9a557d69352601f4c1d84b2
py2048 <quannhn2001@gmail.com>
2022-02-05 13:38:47 +0700

    fix zsh beam

--- zsh/.config/zsh/general.zsh
+++ zsh/.config/zsh/general.zsh
@@ -55,7 +55,7 @@ function zle-keymap-select {
 zle -N zle-keymap-select
 zle-line-init() {
     zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
-    echo -ne "\e[5 q"
+    echo -ne '\e[5 q'
 }
 zle -N zle-line-init
 echo -ne '\e[5 q' # Use beam shape cursor on startup.
