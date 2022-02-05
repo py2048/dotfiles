4c00d3045a01cd74f309f74b8d5e6e6afdfc1b6b
py2048 <quannhn2001@gmail.com>
2022-02-05 16:30:04 +0700

    add julia repl module

---
+++ build/julia/sysimg.jl
@@ -0,0 +1,5 @@
+#!/usr/bin/env julia
+using PackageCompiler
+create_sysimage(["Plots"]; sysimage_path=ENV["HOME"] * "/.local/lib/jlplots.dylib")
+create_sysimage(["GLMakie"]; sysimage_path=ENV["HOME"] * "/.local/lib/jlmakie.dylib")
+create_sysimage(["OhMyREPL"]; sysimage_path=ENV["HOME"] * "/.local/lib/jlrepl.dylib", precompile_statements_file="ohmyrepl_precompile.jl")
