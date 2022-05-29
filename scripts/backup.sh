92ce2194f2aac011e37af133438f4e410501a136
py2048 <quannhn2001@gmail.com>
2022-05-29 21:39:59 +0700

    add scripts

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,4 +2,9 @@
 cd ~/Documents/Backup
 ABSPATH=$(realpath $1)
 BASENAME=$(basename ${ABSPATH})
-cp -R $ABSPATH $BASENAME
+if [ -f "$1" ]; then
+    cp -R $ABSPATH $BASENAME
+elif [ -d "$1" ]; then
+    echo "tar cf - $ABSPATH | 7z a -si $BASENAME.tar.7z"
+    tar cvf - "$ABSPATH" | 7z a -si "$BASENAME".tar.7z
+fi
