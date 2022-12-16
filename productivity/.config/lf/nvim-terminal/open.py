766b713c8fb9f5ea52e19c38b81ed86b24d8e697
py2048 <quannhn2001@gmail.com>
2022-12-16 18:51:11 +0900

    update lf

--- productivity/.config/lf/nvim-terminal/open.py
+++ productivity/.config/lf/nvim-terminal/open.py
@@ -3,6 +3,6 @@ import sys
 from pynvim import attach
 
 nvim = attach('socket', path=os.getenv('NVIM'))
-print(os.getenv('NVIM'))
+# print(os.getenv('NVIM'))
 nvim.command('LfExit')
 nvim.command(f'e {sys.argv[1]}')
