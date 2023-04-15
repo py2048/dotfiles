adc79e88ce6c588ae4044fee796d0d0950246c3c
py2048 <quannhn2001@gmail.com>
2023-04-15 17:55:13 +0900

    update julia sysimg

--- build/julia/sysimg.jl
+++ build/julia/sysimg.jl
@@ -11,9 +11,23 @@ function compile(ext)
             [
                 "Plots",
                 "DataFrames", "CSV",
-                "DifferentialEquations", "MethodOfLines"
+                "DifferentialEquations", "MethodOfLines",
+                "ModelingToolkit", "Symbolics",
+                "FiniteDifferences", "ForwardDiff",
             ];
             sysimage_path=ENV["HOME"] * "/.local/lib/julia/main." * ext)
+        # Makie
+        create_sysimage(
+            [
+                "Makie", "GLMakie", "CairoMakie", "WGLMakie",
+                "DataFrames", "CSV",
+                "DifferentialEquations", "MethodOfLines",
+                "ModelingToolkit", "Symbolics",
+                "FiniteDifferences", "ForwardDiff",
+            ];
+            sysimage_path=ENV["HOME"] * "/.local/lib/julia/makie." * ext)
+        create_sysimage(["PhysicalConstants", "OhMyREPL"];
+            sysimage_path=ENV["HOME"] * "/.local/lib/julia/calc." * ext)
     end
 end
 
