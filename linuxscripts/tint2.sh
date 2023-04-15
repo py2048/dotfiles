9b11fc6bfadedfeae135db2391dcb2719d709477
py2048 <quannhn2001@gmail.com>
2023-04-15 17:59:55 +0900

    tint2 ontop xfce

--- linuxscripts/tint2.sh
+++ linuxscripts/tint2.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 /usr/bin/tint2 &
 sleep 0.5
-wmctrl -r tint2 -b add,above
+# wmctrl -r tint2 -b add,above
+wmctrl -r xfce4-panel -b add,below
 [ $? -eq 0 ] && echo success || echo failed
 
