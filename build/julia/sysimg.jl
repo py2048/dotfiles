4a250348fa63b26160b9e45abae976c9c49e4273
py2048 <quannhn2001@gmail.com>
2023-07-04 00:37:14 +0700

    update julia sysimg build

--- build/julia/sysimg.jl
+++ build/julia/sysimg.jl
@@ -1,22 +1,18 @@
-#!/usr/bin/env julia
+#!/usr/bin/env -S julia -t auto
 using PackageCompiler
 include(ENV["HOME"] * "/.local/lib/julia/parallel.jl")
 
 function compile(ext)
+
+    # Calc
+    create_sysimage(["PhysicalConstants", "OhMyREPL"];
+        sysimage_path=ENV["HOME"] * "/.local/lib/julia/calc." * ext)
+
     @parallel begin
         # Base
-        create_sysimage(["Plots", "OhMyREPL"]; sysimage_path=ENV["HOME"] * "/.local/lib/julia/base." * ext, precompile_statements_file="ohmyrepl_precompile.jl")
+        # create_sysimage(["Plots", "OhMyREPL"]; sysimage_path=ENV["HOME"] * "/.local/lib/julia/base." * ext, precompile_statements_file="ohmyrepl_precompile.jl")
+        create_sysimage(["GLMakie", "Makie"]; sysimage_path=ENV["HOME"] * "/.local/lib/julia/base." * ext)
         # Main
-        create_sysimage(
-            [
-                "Plots",
-                "DataFrames", "CSV",
-                "DifferentialEquations", "MethodOfLines",
-                "ModelingToolkit", "Symbolics",
-                "FiniteDifferences", "ForwardDiff",
-            ];
-            sysimage_path=ENV["HOME"] * "/.local/lib/julia/main." * ext)
-        # Makie
         create_sysimage(
             [
                 "Makie", "GLMakie", "CairoMakie", "WGLMakie",
@@ -25,9 +21,10 @@ function compile(ext)
                 "ModelingToolkit", "Symbolics",
                 "FiniteDifferences", "ForwardDiff",
             ];
-            sysimage_path=ENV["HOME"] * "/.local/lib/julia/makie." * ext)
-        create_sysimage(["PhysicalConstants", "OhMyREPL"];
-            sysimage_path=ENV["HOME"] * "/.local/lib/julia/calc." * ext)
+            sysimage_path=ENV["HOME"] * "/.local/lib/julia/main." * ext)
+        # Plot
+        create_sysimage(["Makie", "CairoMakie", "Pluto", "PlutoUI"]; sysimage_path=ENV["HOME"] * "/.local/lib/julia/plt." * ext)
+
     end
 end
 
