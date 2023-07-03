bd4fad78dca88133075664e0b6546517ba1d9300
py2048 <quannhn2001@gmail.com>
2023-07-04 00:37:54 +0700

    update kmonad services

--- linux/.config/kmonad/kmonad.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-systemctl start --user kbd.service
--- linux/.config/kmonad/src/install.sh
+++ linux/.config/kmonad/src/install.sh
@@ -4,5 +4,3 @@ ln -sf "$PWD/dell3561.kbd" "$(dirname $PWD)/config.kbd"
 
 sed "s/<user>/$USER/g" ./start.sh > ../start.sh
 chmod +x ../start.sh
-sed "s/<user>/$USER/g" ../kbd.service > $HOME/.config/systemd/user/kbd.service
-
